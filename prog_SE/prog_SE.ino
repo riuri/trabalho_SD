1627831a93e2dc8b0ece1c18592b9fa3ef841e6d
Iuri Rezende Souza <riuri@rocketmail.com>
2015-01-18 13:24:51 -0200

    Mudanças do Breno para as boas práticas do prof

--- prog_SE/prog_SE.ino
+++ prog_SE/prog_SE.ino
@@ -1,84 +1,112 @@
-#define NUM_MOTORES 2
-#define ANALOG_MAX 255
-int in1[NUM_MOTORES] = {8, 11};
-int in2[NUM_MOTORES] = {9, 10};
-int vel[NUM_MOTORES] = {0, 0};
-int led = 13;
-int vstep = 32;
+#include <stdin.c>
 
-void setMotorPins()
+const uint8_t u8_AnalogMax = 255;
+const uint8_t u8_NumMotores = 2;
+const uint8_t a_SentidoMotor[u8_NumMotores] = {8, 11};  // in1
+const uint8_t a_PWMMotor[u8_NumMotores] = {9, 10};      // in2
+const uint8_t u8_VelStep = 32;
+const char c_Frente = 'F';
+const char c_Tras = 'B';
+const char c_Direita = 'R';
+const char c_Esquerda = 'L';
+const char c_Parada = 'V', c_ParadaAlt = 'v';
+
+int16_t a_Vel[u8_NumMotores] = {0, 0};
+
+/* Determina os pinos de sentido e velocidade dos motores */
+
+void SetMotorPins()
 {
-  for(int i=0; i<NUM_MOTORES; i++) {
-    pinMode(in1[i], OUTPUT);
-    pinMode(in2[i], OUTPUT);
+  for(int i=0; i<u8_NumMotores; i++)
+  {
+    pinMode(a_SentidoMotor[i], OUTPUT);
+    pinMode(a_PWMMotor[i], OUTPUT);
   }
 }
 
-void setVel(int motor, int vel)
+/* Estabelece os valores para os pinos de sentido e velocidade dos motores */
+
+void SetVel(int motor, int vel)
 {
   int an = vel;
-  if(vel>=0) {
-    an = vel;
-    digitalWrite(in1[motor], 0);
-    analogWrite(in2[motor], an);
-  }
-  else if(vel<0) {
-    an = ANALOG_MAX+vel;
-    digitalWrite(in1[motor], 1);
-    analogWrite(in2[motor], an);
-  }
-}
-
-void alteraVel(int somaA, int somaB)
-{
-  vel[0] += somaA;
-  vel[1] += somaB;
-  for(int i=0; i<NUM_MOTORES; i++) {
-    if(vel[i] > ANALOG_MAX) vel[i] = ANALOG_MAX;
-    else if(vel[i] < -ANALOG_MAX) vel[i] = -ANALOG_MAX;
-    setVel(i, vel[i]);
-  }
-}
-
-void acao(char c)
-{
-  switch(c)
+  if(vel>=0)
   {
-    case 'F': // Para frente
-      alteraVel(+vstep, +vstep);
-      break;
-    case 'B': // Para trás
-      alteraVel(-vstep, -vstep);
-      break;
-    case 'R': // Direita
-      alteraVel(-vstep, +vstep);
-      break;
-    case 'L': // Esquerda
-      alteraVel(+vstep, -vstep);
-      break;
-    case 'V': // Emergência
-    case 'v':
-      alteraVel(-vel[0], -vel[1]);
-      break;
+    an = vel;
+    digitalWrite(a_SentidoMotor[motor], 0);
+    analogWrite(a_PWMMotor[motor], an);
+  }
+  else if(vel<0)
+  {
+    an = u8_AnalogMax+vel;
+    digitalWrite(a_SentidoMotor[motor], 1);
+    analogWrite(a_PWMMotor[motor], an);
   }
 }
 
-void programaBluetooth()
+/* Calcula a nova velocidade de cada motor */
+
+void AlteraVel(int somaA, int somaB)
 {
-  if(Serial.available()>0) {
-    char c = Serial.read();
-    acao(c);
+  a_Vel[0] += somaA;
+  a_Vel[1] += somaB;
+  for(int i=0; i<u8_NumMotores; i++)
+  {
+    if(a_Vel[i] > u8_AnalogMax)
+    {
+      a_Vel[i] = u8_AnalogMax;
+    }
+    else if(a_Vel[i] < -u8_AnalogMax)
+    {
+      a_Vel[i] = -u8_AnalogMax;
+    }
+    SetVel(i, a_Vel[i]);
+  }
+}
+
+/* Altera a velocidade dos motores de acordo com o comando */
+
+void Acao(char comando)
+{
+  switch(comando)
+  {
+    case c_Frente: // Para frente
+    AlteraVel(+u8_VelStep, +u8_VelStep);
+    break;
+    case c_Tras: // Para trás
+    AlteraVel(-u8_VelStep, -u8_VelStep);
+    break;
+    case c_Direita: // Direita
+    AlteraVel(-u8_VelStep, +u8_VelStep);
+    break;
+    case c_Esquerda: // Esquerda
+    AlteraVel(+u8_VelStep, -u8_VelStep);
+    break;
+    case c_Parada: // Emergência
+    case c_ParadaAlt:
+    AlteraVel(-a_Vel[0], -a_Vel[1]);
+    break;
+  }
+}
+
+/* Recebe comando por Bluetooth e executa acao correspondente */
+
+void ProgramaBluetooth()
+{
+  if(Serial.available()>0)
+  {
+    char comando = Serial.read();
+    Acao(comando);
   }
 }
 
 void setup()
 {
-  setMotorPins();
-  pinMode(led, OUTPUT);
+  SetMotorPins();
+//  pinMode(led, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  programaBluetooth();
+  ProgramaBluetooth();
 }
